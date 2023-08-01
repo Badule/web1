b84ff54b001af136acd630839715c2328e328eea
Alexandra - Ionela Cojoc <118635229+Badule@users.noreply.github.com>
2023-08-01 21:53:31 +0300

    Update script-login.php

--- pages/script-login.php
+++ pages/script-login.php
@@ -31,41 +31,14 @@ if (
         $results = mysqli_query($conn,$query);
         if ($results == true)
         {
-        //logging in and sending user to the user dashboard page
-       $em = "am reusit";
+        
+       $em = "Am reusit";
         header("Location: ./home.php?success=$em");
         exit;
         }
-       
-    //     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-    //     $sql = "SELECT * FROM users WHERE reg_email='$email' AND reg_password='$passwordHash'";
-    //     $result = mysqli_query($conn, $sql);
+     
 
-    //     if (password_verify($password, $passwordHash)) {
-    //         $em = "Passss";
-    //         header("Location: ./login.php?error=$em");
-    //         exit;
-    //     } else {
-    //         echo "Password incorrect.";
-    //     }
-    // }
-    //     if (mysqli_num_rows($result) > 0) {
-    // // Parcurgem rezultatele
-    // while ($row = mysqli_fetch_assoc($result)) {
-    //     // Aici poți utiliza valorile din fiecare înregistrare
-    //     echo "ID: " . $row["id"] . ", nume: " . $row["nume"] . ", prenume: " . $row["prenume"] . "Email: " . $row["reg_email"] . "<br>";
-    // }
-// } else {
-//             $em = "Nu s-au găsit înregistrări.";
-//             header("Location: ./login.php?error=$em");
-//             exit;
-    
-// }
 
-// // Închidem conexiunea la baza de date
-// mysqli_close($conn);
-
-//     }
 
  else {
     header("Location: ./login.php?error=error");
